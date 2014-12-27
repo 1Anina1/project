c7f987d11721ba9f53bd3e106c2af9109f73ea69
Amina <annemajel@yandex.ru>
2014-12-27 17:11:03 +0100

    initial

--- src/main/java/lekcja3/program4/Program4.java
+++ src/main/java/lekcja3/program4/Program4.java
@@ -7,11 +7,11 @@ import java.util.Random;
  */
 public class Program4 {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Random random = new Random();         // obiekt potrzebny do losowania
         int[] numbers = new int[10];
 
-        for(int i = 0; i<numbers.length; i++){
+        for (int i = 0; i < numbers.length; i++) {
             numbers[i] = random.nextInt(11);
             System.out.println(numbers[i]);
         }
@@ -19,14 +19,53 @@ public class Program4 {
         int suma = sumaElementow(numbers);
         System.out.println("suma = " + suma);
 
+        int roznica = roznicaZElimetow(numbers);
+        System.out.println("różnica = " + roznica);
+
+        int min = minimumZElementow(numbers);
+        System.out.println("minimum = " + min);
+
+        int max = maximumZElementow(numbers);
+        System.out.println("maximum = " + max);
+
     }
 
-    public static int sumaElementow(int[] numbers){
+    public static int sumaElementow(int[] numbers) {
         int suma = 0;
-        for(int i = 0; i<numbers.length; i++){
+        for (int i = 0; i < numbers.length; i++) {
             suma += numbers[i];               // to samo co suma = suma + numbers[i]
         }
         return suma;
+    }
+
+    public static int minimumZElementow(int[] numbers) {
+        int min = numbers[0];
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] < min) {
+                min = numbers[i];
+            }
+
+        }
+        return min;
+
+    }
+
+    public static int maximumZElementow(int[] numbers) {
+        int max = numbers[0];
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+        }
+        return max;
+    }
+
+    public static int roznicaZElimetow(int[] numbers) {
+        int roznica = numbers[0];
+        for (int i = 1; i < numbers.length; i++) {
+            roznica -= numbers[i];
+        }
+        return roznica;
     }
 
     //TODO napisac dodatkowe metody obliczające:
